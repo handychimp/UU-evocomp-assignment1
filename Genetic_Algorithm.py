7a9a667963510d69c7d0812b86f57729785fa845
cosmiccreepers <tomormsby@gmail.com>
2018-03-11 13:45:08 +0100

    Final experiment and all measures added

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -65,14 +65,22 @@ def crossover( p1, p2, cross_points = 1):
 def family_competition(parents,children,continue_algorithm):      
     #combine family, children first so sort prioritises.
     #sort by descending fitness and take the first 2.
+    selection_error = 0
     family = children + parents
     family.sort(key=itemgetter(1),reverse=True)
     
     for member in children:
         if member in family[0:2]:
             continue_algorithm=True
-            
-    return family[0:2]
+    
+    selection_check1 = set([i for i, x in enumerate(family[0]) if x == 0])
+    selection_check2 = [i for i, x in enumerate(family[0]) if x == 0 and i in selection_check1]        
+    
+    for i in selection_check2:
+        if parents[0][i] == 1 or parents[1][i] == 1:
+            selection_error=selection_error+1
+    
+    return (family[0:2],selection_error)
 
 #4. Apply functions across a population
 #a. Fitness
@@ -96,23 +104,27 @@ def create_children(pop,crossover_function,**kwargs):
 #take in tuples of the member and its fitness
 def selection(parent_pop,child_pop,continue_algorithm,selection_function):
     new_pop=[]
+    selection_errors=0
     continue_algorithm = False
     for i in range(0,int(len(parent_pop)/2)):
         selected = selection_function(parent_pop[2*i:2*i + 2],child_pop[2*i:2*i + 2],continue_algorithm)
-        new_pop.append(selected[0])
-        new_pop.append(selected[1])
-        
-    return new_pop
+        new_pop.append(selected[0][0])
+        new_pop.append(selected[0][1])
+        selection_errors += selected[1]
+    
+    return (new_pop,selection_errors)
 
 def Experiment_UX(seed,pop_size,string_length=100,epochs=1000,**kwargs):
     #Generate Population and Initialise vars
     print('Experiment_UX started...' + str(seed),flush=True)
+    start=time.time()
     local_state = np.random.mtrand.RandomState(seed)
     pop = local_state.randint(2,size=(pop_size,string_length))
     pop_log = []
     continue_algorithm = True
     identical_gen_count = 0
     generation = 0
+    selection_errors = 0
     
     #Loop while children are selected
     while continue_algorithm == True and generation < epochs + 1:
@@ -125,37 +137,42 @@ def Experiment_UX(seed,pop_size,string_length=100,epochs=1000,**kwargs):
      
         child_pop = create_children(pop,crossover)
         child_pop = pop_fitness(child_pop,fitness,**kwargs)
-
-        
-        #Log some stats on the algorithm        
-        total_bits = sum(j for i,j in pop)        
-        pop_log.append((generation,total_bits, total_bits/len(pop)))
     
         #Continue_algorithm flag is evaluated again within this function
         next_pop = selection(pop,child_pop,continue_algorithm,family_competition)
-        
-        if np.array_equal(pop,next_pop):
+        #Log some stats on the algorithm        
+        total_bits = sum(j for i,j in pop)        
+        pop_log.append((generation,total_bits, total_bits/len(pop),selection_errors))        
+
+
+        if np.array_equal(pop,next_pop[0]):
             identical_gen_count += 1
             if identical_gen_count >= 5:
                 continue_algorithm=False
         else:    
-            pop = next_pop
+            pop = next_pop[0]
+            selection_errors = next_pop[1]
             generation +=1
             identical_gen_count = 0
-        
-    print('Experiment_UX started...Trial fitness:' + str(pop_log[len(pop_log)-1][2]),flush=True)
-    return pop_log
+    end = time.time() 
+
+    print('Experiment_UX finished...Trial fitness:' + str(pop_log[len(pop_log)-1][2]),flush=True)
+    #pop_log.append((end-start))
+    return (pop_log,(end-start))
         
 
 
-def Experiment_2X(seed,pop_size,string_length,epochs=1000,**kwargs):
+def Experiment_2X(seed,pop_size,string_length=100,epochs=1000,**kwargs):
     #Generate Population and Initialise vars
-    print('Experiment_UX started...' + str(seed),flush=True)
+    print('Experiment_2X started...' + str(seed),flush=True)
+    start=time.time()
     local_state = np.random.mtrand.RandomState(seed)
     pop = local_state.randint(2,size=(pop_size,string_length))
     pop_log = []
     continue_algorithm = True
+    identical_gen_count = 0
     generation = 0
+    selection_errors = 0
     
     #Loop while children are selected
     while continue_algorithm == True and generation < epochs + 1:
@@ -168,18 +185,25 @@ def Experiment_2X(seed,pop_size,string_length,epochs=1000,**kwargs):
      
         child_pop = create_children(pop,crossover,cross_points=2)
         child_pop = pop_fitness(child_pop,fitness,**kwargs)
-               
-        #Log some stats on the algorithm        
-        total_bits = sum(j for i,j in pop)        
-        pop_log.append((generation,total_bits, total_bits/len(pop)))
-
+    
         #Continue_algorithm flag is evaluated again within this function
         next_pop = selection(pop,child_pop,continue_algorithm,family_competition)
-        
-        if np.array_equal(pop,next_pop):
-            continue_algorithm = False
+        #Log some stats on the algorithm        
+        total_bits = sum(j for i,j in pop)        
+        pop_log.append((generation,total_bits, total_bits/len(pop),selection_errors))        
+
+
+        if np.array_equal(pop,next_pop[0]):
+            identical_gen_count += 1
+            if identical_gen_count >= 5:
+                continue_algorithm=False
         else:    
-            pop = next_pop
+            pop = next_pop[0]
+            selection_errors = next_pop[1]
             generation +=1
-        print ('Trial fitness: ' + str(pop_log[len(pop_log)-1][2]))
-    return pop_log
+            identical_gen_count = 0
+    end = time.time() 
+
+    print('Experiment_2X finished...Trial fitness:' + str(pop_log[len(pop_log)-1][2]),flush=True)
+    #pop_log.append((end-start))
+    return (pop_log,(end-start))
--- Main.py
+++ Main.py
@@ -9,13 +9,12 @@ Created on Mon Mar  5 00:41:59 2018
 
 from __future__ import print_function
 import Genetic_Algorithm as ga
-import numpy as np
 import sys
 from multiprocessing import Pool
 from functools import partial
-import multiprocessing as mp
 import pickle
-
+import time
+import numpy as np
 
 if sys.version_info[:2] < (3, 3):
     old_print = print
@@ -28,51 +27,9 @@ if sys.version_info[:2] < (3, 3):
             file.flush() if file is not None else sys.stdout.flush()
 
 #Minimum Population Functions
-def Minimum_Population_UX(**kwargs):
-    UX_success_counter = 0
-    pop_size = 10
-    UX_results = []
-    while UX_success_counter < 24 and pop_size <= 1280:
-        print(pop_size,flush=True)
-        Trial_results = []
-        UX_success_counter = 0
-        for i in range(0,25):
-            Trial_results.append(ga.Experiment_UX(pop_size,100,**kwargs))
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                UX_success_counter += 1        
-            print('Trial ' + str(i) + ' gives fitness: ' + str(Trial_results[i][len(Trial_results[i])-1][2]),flush=True)
-    
-        UX_results.append((pop_size,Trial_results))
-        if UX_success_counter < 24:
-            pop_size = pop_size * 2
-    
-    UX_success_counter = 0
-    a = pop_size
-    b = int(pop_size/2)
-    pop_size = int((a+b)/2)
-    while a-b >= 10:
-        print(pop_size,flush=True)
-        Trial_results = []
-        UX_success_counter = 0
-        for i in range(0,25):
-            Trial_results.append(ga.Experiment_UX(pop_size,100,**kwargs))
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                UX_success_counter += 1
-                print('Trial ' + str(i) + ' gives fitness: ' + str(Trial_results[i][len(Trial_results[i])-1][2]),flush=True)
-        
-        UX_results.append((pop_size,Trial_results))    
-        if UX_success_counter >= 24:
-            a = pop_size
-            pop_size = int((a+b)/2)
-        else:
-            b = pop_size
-            pop_size = int((a+b)/2)
-    return UX_results
-
-def Seeded_Experiment(seed,**kwargs):
-    return partial(ga.Experiment_UX,pop_size,string_length=100,**kwargs)
 
 def Minimum_Population_UX_Parallel(**kwargs):
+ 
     UX_success_counter = 0
     pop_size = 10
     UX_results = []
@@ -89,93 +46,47 @@ def Minimum_Population_UX_Parallel(**kwargs):
         UX_results.append((pop_size,Trial_results))
         
         for i in range (0,len(Trial_results)):
-            if int(Trial_results[i][len(Trial_results[i])-1][2])==100:
+            if int(Trial_results[i][0][len(Trial_results[i])-1][2])==100:
                 UX_success_counter += 1
         print('Successes: ' + str(UX_success_counter) + ' out of 25')
         
         if UX_success_counter < 24:
             pop_size = pop_size * 2
     
-    UX_success_counter = 0
-    a = pop_size
-    b = int(pop_size/2)
-    pop_size = int((a+b)/2)
-    while a-b >= 10:
-        print(pop_size,flush=True)
-        Trial_results = []
+    if UX_success_counter < 24:
+        print('Maximum Population Size Reached')
+    else:
         UX_success_counter = 0
-        map_func = partial(ga.Experiment_UX,pop_size=pop_size,string_length=100,**kwargs)
-        p=Pool()
-        Trial_results=p.map(map_func,range(0,25))
-        p.close()
-        p.join()
-        UX_results.append((pop_size,Trial_results))
+        a = pop_size
+        b = int(pop_size/2)
+        pop_size = int((a+b)/2)
+        while a-b >= 10:
+            print(pop_size,flush=True)
+            Trial_results = []
+            UX_success_counter = 0
+            map_func = partial(ga.Experiment_UX,pop_size=pop_size,string_length=100,**kwargs)
+            p=Pool()
+            Trial_results=p.map(map_func,range(0,25))
+            p.close()
+            p.join()
+            UX_results.append((pop_size,Trial_results))
         
-        for i in range (0,len(Trial_results)):
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                UX_success_counter += 1
-        print('Successes: ' + str(UX_success_counter) + ' out of 25')
-        if UX_success_counter >= 24:
-            a = pop_size
-            pop_size = int((a+b)/2)
-        else:
-            b = pop_size
-            pop_size = int((a+b)/2)
+            for i in range (0,len(Trial_results)):
+                if Trial_results[i][0][len(Trial_results[i])-1][2]==100:
+                    UX_success_counter += 1
+            print('Successes: ' + str(UX_success_counter) + ' out of 25')
+            if UX_success_counter >= 24:
+                a = pop_size
+                pop_size = int((a+b)/2)
+            else:
+                b = pop_size
+                pop_size = int((a+b)/2)
+    
     return UX_results
 
 
 
 #a Uniform Crossover
-def Minimum_Population_2X(**kwargs):
-    TWOX_success_counter = 0
-    pop_size = 10
-    TWOX_results = []
-    while TWOX_success_counter < 24 and pop_size <= 1280:
-        print(pop_size,flush=True)
-        Trial_results = []
-        TWOX_success_counter = 0
-        for i in range(0,25):
-            Trial_results.append(ga.Experiment_2X(pop_size=pop_size,string_length=100,**kwargs))
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                TWOX_success_counter += 1        
-            print('Trial ' + str(i) + ' gives fitness: ' + str(Trial_results[i][len(Trial_results[i])-1][2]),flush=True)
-    
-        TWOX_results.append((pop_size,Trial_results))
-        if TWOX_success_counter < 24:
-            pop_size = pop_size * 2
-    
-    TWOX_success_counter = 0
-    a = pop_size
-    b = int(pop_size/2)
-    pop_size = int((a+b)/2)
-    while a-b >= 10:
-        print(pop_size,flush=True)
-        Trial_results = []
-        TWOX_success_counter = 0
-        for i in range(0,25):
-            Trial_results=ga.Experiment_2X(pop_size,100,**kwargs)
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                TWOX_success_counter += 1
-                print('Trial ' + str(i) + ' gives fitness: ' + str(Trial_results[i][len(Trial_results[i])-1][2]),flush=True)
-        
-        TWOX_results.append((pop_size,Trial_results))    
-        if TWOX_success_counter >= 24:
-            a = pop_size
-            pop_size = int((a+b)/2)
-        else:
-            b = pop_size
-            pop_size = int((a+b)/2)
-    return TWOX_results
-
-def test_fun(**kwargs):
-    pop_size = 10
-    map_func = partial(ga.Experiment_2X,pop_size=pop_size,string_length=100,**kwargs)
-    return map_func
-
-test = test_fun(trap=True)
-test.keywords
-test.args
-
 def Minimum_Population_2X_Parallel(**kwargs):
     TWOX_success_counter = 0
     pop_size = 10
@@ -185,7 +96,6 @@ def Minimum_Population_2X_Parallel(**kwargs):
         print(pop_size,flush=True)
         Trial_results = []
         TWOX_success_counter = 0
-        pop_size = 10
         map_func = partial(ga.Experiment_2X,pop_size=pop_size,string_length=100,**kwargs)
         p=Pool()
         Trial_results=p.map(map_func,range(0,25))
@@ -195,54 +105,95 @@ def Minimum_Population_2X_Parallel(**kwargs):
         TWOX_results.append((pop_size,Trial_results))
         
         for i in range (0,len(Trial_results)):
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
+            if Trial_results[i][0][len(Trial_results[i])-1][2]==100:
                 TWOX_success_counter += 1
         print('Successes: ' + str(TWOX_success_counter) + ' out of 25')
         
         if TWOX_success_counter < 24:
             pop_size = pop_size * 2
     
-    TWOX_success_counter = 0
-    a = pop_size
-    b = int(pop_size/2)
-    pop_size = int((a+b)/2)
-    while a-b >= 10:
-        print(pop_size,flush=True)
-        Trial_results = []
+    if TWOX_success_counter < 24:
+        print('Maximum Population Size Reached')
+    else:
         TWOX_success_counter = 0
-        p=Pool()
-        Trial_results=p.map(map_func,range(0,25))
-        p.close()
-        p.join()
-        TWOX_results.append((pop_size,Trial_results))
-        
-        for i in range (0,len(Trial_results)):
-            if Trial_results[i][len(Trial_results[i])-1][2]==100:
-                TWOX_success_counter += 1
-        print('Successes: ' + str(TWOX_success_counter) + ' out of 25')  
-        if TWOX_success_counter >= 24:
-            a = pop_size
-            pop_size = int((a+b)/2)
-        else:
-            b = pop_size
-            pop_size = int((a+b)/2)
+        a = pop_size
+        b = int(pop_size/2)
+        pop_size = int((a+b)/2)
+        while a-b >= 10:
+            print(pop_size,flush=True)
+            Trial_results = []
+            TWOX_success_counter = 0
+            map_func = partial(ga.Experiment_2X,pop_size=pop_size,string_length=100,**kwargs)
+            p=Pool()
+            Trial_results=p.map(map_func,range(0,25))
+            p.close()
+            p.join()
+            TWOX_results.append((pop_size,Trial_results))
+            
+            for i in range (0,len(Trial_results)):
+                if Trial_results[i][0][len(Trial_results[i])-1][2]==100:
+                    TWOX_success_counter += 1
+            print('Successes: ' + str(TWOX_success_counter) + ' out of 25')  
+            if TWOX_success_counter >= 24:
+                a = pop_size
+                pop_size = int((a+b)/2)
+            else:
+                b = pop_size
+                pop_size = int((a+b)/2)
     return TWOX_results
 
 
-def Random_Linking_Funcs(x):
-    if x == 1:    
-        results = Minimum_Population_UX(trap=True,random_linked=True)
-    elif x==2:
-        results = Minimum_Population_2X(trap=True,random_linked=True)
+def Fitness_Experiment(x):
+    if x == 0:    
+        results = ga.Experiment_UX(seed=1,pop_size=250)
+    elif x==1:
+        results = ga.Experiment_2X(seed=1,pop_size=250)
+    elif x == 2:
+        results = ga.Experiment_UX(seed=1,pop_size=250,trap=True)
     elif x == 3:
-        results = Minimum_Population_UX(trap=True,random_linked=True,d=2.5)
+        results = ga.Experiment_2X(seed=1,pop_size=250,trap=True)
     elif x == 4:
-        results = Minimum_Population_UX(trap=True,random_linked=True,d=2.5)
+        results = ga.Experiment_UX(seed=1,pop_size=250,trap=True,d=2.5)
+    elif x == 5:
+        results = ga.Experiment_2X(seed=1,pop_size=250,trap=True,d=2.5)
+    elif x == 6:
+        results = ga.Experiment_UX(seed=1,pop_size=250,trap=True,random_linked=True)
+    elif x == 7:
+        results = ga.Experiment_2X(seed=1,pop_size=250,trap=True,random_linked=True)
+    elif x== 8:
+        results = ga.Experiment_UX(seed=1,pop_size=250,trap=True,random_linked=True,d=2.5)
+    elif x==9:
+        results = ga.Experiment_2X(seed=1,pop_size=250,trap=True,random_linked=True,d=2.5)
 
     return results    
 
 if __name__ == '__main__':
     __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
+        
+    Results_CountOnes_UX = Minimum_Population_UX_Parallel()
+    with open('CountOnes_UX','wb') as fp:
+        pickle.dump(Results_CountOnes_UX,fp)
+        
+    Results_CountOnes_2X = Minimum_Population_2X_Parallel()
+    with open('CountOnes_2X','wb') as fp:
+        pickle.dump(Results_CountOnes_2X,fp)
+    
+    Results_DecepTrap_UX = Minimum_Population_UX_Parallel(trap=True)
+    with open('Deceptive_UX','wb') as fp:
+        pickle.dump(Results_DecepTrap_UX,fp)
+    
+    Results_DecepTrap_2X = Minimum_Population_2X_Parallel(trap=True)
+    with open('Deceptive_2X','wb') as fp:
+        pickle.dump(Results_DecepTrap_2X,fp)
+    
+    Results_NonDecepTrap_UX = Minimum_Population_UX_Parallel(trap=True,d=2.5)
+    with open('NonDeceptive_UX','wb') as fp:
+        pickle.dump(Results_NonDecepTrap_UX,fp)
+    
+    Results_NonDecepTrap_2X = Minimum_Population_2X_Parallel(trap=True,d=2.5)
+    with open('NonDeceptive_UX','wb') as fp:
+        pickle.dump(Results_NonDecepTrap_2X,fp)
+    
     Results_Random_Deceptive_Trap_UX = Minimum_Population_UX_Parallel(trap=True,random_linked=True)
     with open('Random_Deceptive_UX','wb') as fp:
         pickle.dump(Results_Random_Deceptive_Trap_UX,fp)
@@ -259,21 +210,9 @@ if __name__ == '__main__':
     with open('Random_NonDeceptive_2X','wb') as fp:
         pickle.dump(Results_Random_NonDeceptive_Trap_2X,fp) 
         
-  
-    
-    #Results_Random_Deceptive_Trap_2X = Minimum_Population_2X(trap=True,random_linked=True)
-    #Results_Random_NonDeceptive_Trap_UX = Minimum_Population_UX(trap=True,random_linked=True,d=2.5)
-    #Results_Random_NonDeceptive_Trap_2X = Minimum_Population_UX(trap=True,random_linked=True,d=2.5)
-
-    #Results_CountOnes_UX = Minimum_Population_UX()
-    #Results_CountOnes_2X = Minimum_Population_2X()
-    #Results_DecepTrap_UX = Minimum_Population_UX(trap=True)
-    #Results_DecepTrap_2X = Minimum_Population_2X(trap=True)
-    #Results_NonDecepTrap_UX = Minimum_Population_UX(trap=True,d=2.5)
-    #Results_NonDecepTrap_2X = Minimum_Population_2X(trap=True,d=2.5)
-
-
-#os.getpid()
-
-
-                
+    p=Pool()
+    Fitness_results=p.map(Fitness_Experiment,range(0,10))
+    p.close()
+    p.join()
+    with open('Fitness_Results_250Pop','wb') as fp:
+        pickle.dump(Fitness_results,fp) 
